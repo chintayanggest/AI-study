8c599eab9b75ef7039114c929fab747f457812e1
Chinta Yanggest Br Tarigan <chintacapric836@gmail.com>
2025-08-05 11:51:34 +0700

    phase 1 day 1 learning variables and data types

---
+++ day2/variables&datatypes.py
@@ -0,0 +1,5 @@
+name = input("What is your name : ")
+age = input("What is your age : ")
+favorite_color = input("What is your favorite color : ")
+
+print(f"Hi My name is {name} , I'm {age} years old and my favorite color is {favorite_color}")
